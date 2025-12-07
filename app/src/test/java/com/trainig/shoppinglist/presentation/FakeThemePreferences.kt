af72bbcf294eba4679595d8c565965cd694abc64
gaborweisz <gaborweisz@gmail.com>
2025-12-07 20:55:46 +0100

    fixed unit tests

--- app/src/test/java/com/trainig/shoppinglist/presentation/FakeThemePreferences.kt
+++ app/src/test/java/com/trainig/shoppinglist/presentation/FakeThemePreferences.kt
@@ -6,10 +6,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 class FakeThemePreferences : ThemePreferences {
     private val _isDarkMode = MutableStateFlow<Boolean?>(null)
+    private val _language = MutableStateFlow<String?>(null)
 
     override val isDarkMode: Flow<Boolean?> = _isDarkMode
+    override val language: Flow<String?> = _language
 
     override suspend fun setDarkMode(isDark: Boolean) {
         _isDarkMode.value = isDark
+    }
+
+    override suspend fun setLanguage(languageCode: String) {
+        _language.value = languageCode
     }
 }
